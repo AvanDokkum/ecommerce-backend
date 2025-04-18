37a6f568252641013b11e2b0e605ea9d8b1821c4
avandokkum <arjanvandokkum@gmail.com>
2025-04-18 13:38:45 +0200

    Initial commit

---
+++ src/main/java/com/webcanis/ecommerce_backend/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.webcanis.ecommerce_backend.service;
+
+import com.webcanis.ecommerce_backend.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
+
+}
