e142d397adb24ce93363b0639970ce29a07fd53a
avandokkum <arjanvandokkum@gmail.com>
2025-04-21 14:16:10 +0200

    added CategoryRepository

--- src/main/java/com/webcanis/backend/repositories/CategoryRepository.java
+++ src/main/java/com/webcanis/backend/repositories/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.webcanis.backend.repositories;
 
-public interface CategoryRepository {
+import com.webcanis.backend.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//@Repository //?no @Repository needed for this repository?
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
