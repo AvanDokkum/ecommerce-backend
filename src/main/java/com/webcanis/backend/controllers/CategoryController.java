f6a4b017fcd242dc3520b92be286d3c20e6739f6
avandokkum <arjanvandokkum@gmail.com>
2025-05-11 23:46:15 +0200

    working on pagination in getAllCategories method

--- src/main/java/com/webcanis/backend/controllers/CategoryController.java
+++ src/main/java/com/webcanis/backend/controllers/CategoryController.java
@@ -20,14 +20,16 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/echo")
-    public ResponseEntity<String> echoMessage(@RequestParam(name = "message") String message){
+    public ResponseEntity<String> echoMessage(@RequestParam(name = "message", defaultValue = "Hello World") String message) {
         return new ResponseEntity<>("Echoed message: " + message, HttpStatus.OK);
     }
 
     //    @GetMapping("/api/public/categories")
     @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
-    public ResponseEntity<CategoryResponse> getAllCategories() {
-        CategoryResponse categoryResponse = categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(
+            @RequestParam(name = "pageNumber") Integer pageNumber,
+            @RequestParam(name = "pageSize") Integer pageSize) {
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
@@ -41,18 +43,18 @@ public class CategoryController {
     //    @DeleteMapping("/admin/categories/{categoryId}")
     @RequestMapping(value = "/admin/categories/{categoryId}", method = RequestMethod.DELETE)
     public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
-            CategoryDTO deletedCategoryDTO = categoryService.deleteCategory(categoryId);
+        CategoryDTO deletedCategoryDTO = categoryService.deleteCategory(categoryId);
 //            multiple ways of doing declaring the same response entity
 //            return ResponseEntity.ok(status);
 //            return ResponseEntity.status(HttpStatus.OK).body(status);
-            return new ResponseEntity<>(deletedCategoryDTO, HttpStatus.OK);
+        return new ResponseEntity<>(deletedCategoryDTO, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/public/categories/{categoryId}", method = RequestMethod.PUT)
     public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO,
-                                                 @PathVariable Long categoryId) {
-            CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
-            return new ResponseEntity<>(savedCategoryDTO, HttpStatus.OK);
+                                                      @PathVariable Long categoryId) {
+        CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
+        return new ResponseEntity<>(savedCategoryDTO, HttpStatus.OK);
     }
 
 
