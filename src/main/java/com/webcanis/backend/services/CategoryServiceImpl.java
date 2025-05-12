5c5b744a020d00485a2221840863bb88d2c512a3
avandokkum <arjanvandokkum@gmail.com>
2025-05-12 23:08:54 +0200

    basic pagination implementation

--- src/main/java/com/webcanis/backend/services/CategoryServiceImpl.java
+++ src/main/java/com/webcanis/backend/services/CategoryServiceImpl.java
@@ -8,6 +8,9 @@ import com.webcanis.backend.payload.CategoryResponse;
 import com.webcanis.backend.repositories.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,7 +27,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
-        List<Category> categories = categoryRepository.findAll();
+
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize);
+        Page<Category> categoryPage = categoryRepository.findAll(pageDetails);
+        List<Category> categories = categoryPage.getContent();
 
         if (categories.isEmpty()) {
             throw new APIException("No category created yet.");
